1e966836e1123619baaa01fae1645c52f2aa6a0b
Vasily Esipenko <esipenkovasilij@gmail.com>
2020-06-05 12:24:47 +0300

    DivisibleBy JavaScript solution

--- JavaScript/divisible.js
+++ JavaScript/divisible.js
@@ -1,10 +1,19 @@
 // 8kyu kata
 // Find numbers which are divisible by given number
 // link: https://www.codewars.com/kata/55edaba99da3a9c84000003b
+// Description:
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. 
+// First argument is an array of numbers and the second is the divisor.
 
-//Solution:
+
+//My solution:
+
 
 function divisibleBy(numbers, divisor){
     let result = numbers.filter(num => num % divisor == 0);
     return result;
 }
+
+
+// My profile on Codewars: https://www.codewars.com/users/vasilyesp
+// My StackOverflow account: https://stackoverflow.com/users/13187161/vasily-esipenko
