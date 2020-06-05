f21f4a7922d804d324d1f4271279ffc7638837c6
Vasily Esipenko <esipenkovasilij@gmail.com>
2020-06-05 12:23:27 +0300

    FindIt Python solution

--- Python/findit.py
+++ Python/findit.py
@@ -1,9 +1,20 @@
 # 6kyu kata
 # Find the odd int
 # Link: https://www.codewars.com/kata/54da5a58ea159efa38000836
+# Description:
+# Given an array, find the integer that appears an odd number of times.
+#There will always be only one integer that appears an odd number of times.
+
+
+# My solution:
+
 
 def find_it(seq):
     for i in seq:
         seq.count(i)
         if seq.count(i) % 2 != 0:
             return i
+
+
+# My profile on Codewars: https://www.codewars.com/users/vasilyesp
+# My StackOverflow account: https://stackoverflow.com/users/13187161/vasily-esipenko
