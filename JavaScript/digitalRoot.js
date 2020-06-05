e798b795dc312be84abf5e608ac94477e50c4de8
Vasily Esipenko <esipenkovasilij@gmail.com>
2020-06-05 12:33:00 +0300

    DigitalRoot JavaScript solution

---
+++ JavaScript/digitalRoot.js
@@ -0,0 +1,20 @@
+// 6kyu kata
+// Sum of Digits / Digital Root
+// link: https://www.codewars.com/kata/541c8630095125aba6000c00
+// Description:
+// Digital root is the recursive sum of all the digits in a number.
+// Given n, take the sum of the digits of n. 
+// If that value has more than one digit, continue reducing in this way until a single-digit number is produced. 
+// This is only applicable to the natural numbers.
+
+
+// My solution:
+
+
+function digital_root(n) {
+    return (n - 1) % 9 + 1;
+  }
+
+
+// My profile on Codewars: https://www.codewars.com/users/vasilyesp
+// My StackOverflow account: https://stackoverflow.com/users/13187161/vasily-esipenko
